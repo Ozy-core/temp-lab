c04b9d5e6e45aef6101e85c342727146615dc840
Osier Nono <osiernono@gmail.com>
2025-10-07 20:32:28 -0500

    starting out the code by adding everything i need. Such as the tempaltes, parameters and the template funtions etc

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+template <class T>
+T vec_fill(T elem)
+{
+
+}
+
+template <class T>
+T until(T elem,function<T(T)> func, function<bool(T)> predicate)
+{
+
+}
+
+template <class T>
+T order(T& a, T& b, T& c)
+{
+
+}
+
+int main()
+{
+    
+}
